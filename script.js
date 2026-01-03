82a09c475a56657fe3613827d9da294d304ad4a1
Sandeep Ambig <assandeep95@gmail.com>
2026-01-03 15:35:06 +0530

    files updated

--- script.js
+++ script.js
@@ -4,49 +4,49 @@ let products = {
             productName :"Regular White T-shirt",
             category : "Topwear",
             price : "180" ,
-            image : "white-tshirt.jpg"
+            image : "images/white-tshirt.jpg"
         },
         {
             productName :"Beige Short Skirt",
             category : "Bottomwear",
             price : "180" ,
-            image : "short-skirt.jpg"
+            image : "images/short-skirt.jpg"
         },
         {
             productName :"Sporty Smartwatch",
             category : "Watch",
             price : "180" ,
-            image : "sporty-smartwatch.jpg"
+            image : "images/sporty-smartwatch.jpg"
         },
         {
             productName :"Basic Knitted Top",
             category : "Topwear",
             price : "180" ,
-            image : "Knitted-top.jpg"
+            image : "images/Knitted-top.jpg"
         },
         {
             productName :"Black Leather Jacket",
             category : "Jacket",
             price : "180" ,
-            image : "black-leather-jacket.jpg"
+            image : "images/black-leather-jacket.jpg"
         },
         {
             productName :"Stylish Pink Trouser",
             category : "Bottomwear",
             price : "180" ,
-            image : "pink-trousers.jpg"
+            image : "images/pink-trousers.jpg"
         },
         {
             productName :"Brown Mens Jacket",
             category : "Jacket",
             price : "180" ,
-            image : "brown-jacket.jpg"
+            image : "images/brown-jacket.jpg"
         },
         {
             productName :"Comfy Gray Pants",
             category : "Bottomwear",
             price : "180" ,
-            image : "comfy-gray-pants.jpg"
+            image : "images/comfy-gray-pants.jpg"
         },
     ],
 };
@@ -54,4 +54,65 @@ let products = {
 for(let i of products.data){
 
    let card = document.createElement("div");
+   card.classList.add("card",i.category,"hide");
+
+   let imageContainer = document.createElement("div");
+   imageContainer.classList.add("image-container");
+
+   let image = document.createElement("img");
+   image.setAttribute("src",i.image);
+
+   imageContainer.appendChild(image);
+   card.appendChild(imageContainer);
+
+   let container = document.createElement("div");
+   container.classList.add("container");
+
+   let name = document.createElement("h5");
+   name.classList.add("product-name");
+   name.innerText = i.productName.toUpperCase() ;
+   container.appendChild(name);
+
+   let price = document.createElement("h6");
+   price.innerText = "$"+i.price ;
+   container.appendChild(price);
+
+   card.appendChild(container);
+
+   document.getElementById("products").appendChild(card);
+}
+
+
+function filterProduct(value){
+let buttons = document.querySelectorAll('.button-value');
+
+buttons.forEach((button)=>{
+    if(value.toUpperCase() == button.innerText.toUpperCase()){
+        button.classList.add('active');
+    }
+    else {
+        button.classList.remove('active');
+    }
+});
+
+    let elements = document.querySelectorAll('.card');
+
+    elements.forEach((element)=>{
+
+        if(value.toUpperCase() == 'ALL'){
+            element.classList.remove('hide');
+        }
+        else {
+            if(element.classList.contains(value)){
+                element.classList.remove('hide');
+            }
+            else {
+                element.classList.add('hide');
+            }
+        }
+    })
+}
+//initially display all products
+window.onload = ()=>{
+    filterProduct('All');
 }
