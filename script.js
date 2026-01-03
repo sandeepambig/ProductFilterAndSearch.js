08e4e6f0a1c9e791eb6b3603b27f1a12a0a38e09
Sandeep Ambig <assandeep95@gmail.com>
2026-01-03 13:21:02 +0530

    files updated

---
+++ script.js
@@ -0,0 +1,57 @@
+let products = {
+    data : [
+        {
+            productName :"Regular White T-shirt",
+            category : "Topwear",
+            price : "180" ,
+            image : "white-tshirt.jpg"
+        },
+        {
+            productName :"Beige Short Skirt",
+            category : "Bottomwear",
+            price : "180" ,
+            image : "short-skirt.jpg"
+        },
+        {
+            productName :"Sporty Smartwatch",
+            category : "Watch",
+            price : "180" ,
+            image : "sporty-smartwatch.jpg"
+        },
+        {
+            productName :"Basic Knitted Top",
+            category : "Topwear",
+            price : "180" ,
+            image : "Knitted-top.jpg"
+        },
+        {
+            productName :"Black Leather Jacket",
+            category : "Jacket",
+            price : "180" ,
+            image : "black-leather-jacket.jpg"
+        },
+        {
+            productName :"Stylish Pink Trouser",
+            category : "Bottomwear",
+            price : "180" ,
+            image : "pink-trousers.jpg"
+        },
+        {
+            productName :"Brown Mens Jacket",
+            category : "Jacket",
+            price : "180" ,
+            image : "brown-jacket.jpg"
+        },
+        {
+            productName :"Comfy Gray Pants",
+            category : "Bottomwear",
+            price : "180" ,
+            image : "comfy-gray-pants.jpg"
+        },
+    ],
+};
+
+for(let i of products.data){
+
+   let card = document.createElement("div");
+}
